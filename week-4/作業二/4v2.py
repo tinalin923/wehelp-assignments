cc9077e11b3dd2c8355ee80d06288b65bc07b07d
tinalin923 <tinalin923@gmail.com>
2022-01-21 02:02:29 +0800

    add session / query string

--- week-4/作業二/4v2.py
+++ week-4/作業二/4v2.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for,session
 
 app=Flask(
     __name__,
     template_folder="./templates",
     static_folder="./static",
     static_url_path="/")  
-
+app.secret_key="123654789"
 @app.route("/")      
 def home():
     return render_template('4v1.html')
@@ -14,27 +14,27 @@ def home():
 def sign():
     account= request.form["account"]
     password= request.form["password"]
+    session["account"]=request.form["account"]
+    session["password"]=request.form["password"]
     if account=="test" and password=="test":
         return redirect("/member/")
     if account=="" or password=="":
-        return redirect("/error/?message=請輸入帳號、密碼")   #想直接導入該網址xd 失敗~
+        return redirect(url_for("error",message="請輸入帳號、密碼"))
     else:
-        return redirect("/error/?message=帳號、或密碼輸入錯誤")
+        return redirect(url_for("error",message="帳號、或密碼輸入錯誤"))
     
 @app.route("/member/",methods=["GET"])
 def member():
     return render_template('member.html')
 
-@app.route("/error/?message=請輸入帳號、密碼")
-def please():
-    return render_template("error.html",message="請輸入帳號、密碼")
+@app.route("/error/")
+def error():
+    a=session.get("account",None)
+    p=session.get("password",None)
+    if a=="" or p=="":
+        return render_template("error.html",message="請輸入帳號、密碼") 
+    else:
+        return render_template("error.html",message="帳號、或密碼輸入錯誤")
 
-
-@app.route("/error/?message=帳號、或密碼輸入錯誤")
-def wrong():
-    return render_template("error.html",message="帳號、或密碼輸入錯誤")
-
-
-
-if __name__=="__main__":      #  如果以主程式執行
-    app.run(port="3000",debug=True)               # 立刻啟動伺服器
+if __name__=="__main__":     
+    app.run(port="3000",debug=True)         
