461413e384079cbfb82a7d89722433f1df8971c1
林怡君 <94776718+tinalin923@users.noreply.github.com>
2022-02-18 17:54:53 +0800

    Delete db.py

--- week-7/db.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector                                  
-connection= mysql.connector.connect(
-    host='localhost', 
-    port='3306',
-    user='root', 
-    password='toor',
-    database='website')  
