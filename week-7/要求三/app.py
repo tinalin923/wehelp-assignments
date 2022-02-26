000969f8f11f3bcd10343ec4f0a5e97d227cc7bd
tinalin923 <tinalin923@gmail.com>
2022-02-26 22:57:19 +0800

    delete redundant bp

--- week-7/要求三/app.py
+++ week-7/要求三/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from home.views import home_bp
 from login.views import login_bp
 from register.views import register_bp
-from api.views import api_bp
+
 
 def create_app():    
     app=Flask(__name__)                                      
@@ -12,7 +12,7 @@ def create_app():
     app.register_blueprint(home_bp)
     app.register_blueprint(login_bp)        
     app.register_blueprint(register_bp)     
-    app.register_blueprint(api_bp)
+   
     
     return app
     
